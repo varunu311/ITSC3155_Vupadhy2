9bb8d72e1361734cc1ea90833ead5a2a64ffaad2
Varun <vupadhy2@uncc.edu>
2022-09-22 23:26:20 -0400

    PythonBasics 2 - added logic to count_threes function

--- PythonBasics2/pythonBasics2.py
+++ PythonBasics2/pythonBasics2.py
@@ -11,18 +11,40 @@
 # to n (including n).
 
 def count_threes(n):
-  # YOUR CODE HERE
+  x = 0
+  if n < 3 and n>-3:
+    return 0
+  else:
+    for i in range(0,n):
+      if i%3 == 0:
+        x = x+1
+      else:
+        pass
 
-  return
+  return x
 
 
 # Part B. longest_consecutive_repeating_char
 # Define a function longest_consecutive_repeating_char(s) that takes
 # a string s and returns the character that has the longest consecutive repeat.
 def longest_consecutive_repeating_char(s):
-  # YOUR CODE HERE
 
-  return
+  last_char = ""
+  current_seq_len = 0
+  max_seq_len = 0
+  max_char = ''
+
+  for c in s:
+    if c == last_char:
+      current_seq_len += 1
+      if current_seq_len > max_seq_len:
+        max_seq_len = current_seq_len
+        max_char = c
+    else:
+      current_seq_len = 1
+      last_char = c
+
+  return max_char
 
 
 # Part C. is_palindrome
@@ -32,6 +54,14 @@ def longest_consecutive_repeating_char(s):
 # forwards. Treat capital letters the same as lowercase ones
 # and ignore spaces (i.e. case insensitive).
 def is_palindrome(s):
-  # YOUR CODE HERE
 
-  return
+  string = s.lower
+  fliped = string[::-1]
+  print (fliped)
+  tf = False
+
+  if fliped == string:
+    tf = True
+    return tf
+  else:
+    return tf
