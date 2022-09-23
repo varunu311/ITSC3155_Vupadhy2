6f5a7f1165a9585d6e7e6e52a76475ed515e6e6c
Varun <vupadhy2@uncc.edu>
2022-09-22 23:28:32 -0400

    PythonBasics 2 - added logic to longest_consecutive_repeating_char function

--- PythonBasics2/pythonBasics2Test.py
+++ PythonBasics2/pythonBasics2Test.py
@@ -13,7 +13,7 @@ def test(got, expected):
 def main():
     # set which functions to test
     check_count_threes = True
-    check_longest_consecutive_repeating_char = False
+    check_longest_consecutive_repeating_char = True
     check_is_palindrome = False
 
     if check_count_threes:
