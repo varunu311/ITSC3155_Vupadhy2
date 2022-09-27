0107e4fe9652fe96990c4b326bff3b7e3841c78a
Varun <vupadhy2@uncc.edu>
2022-09-27 13:30:25 -0400

    PythonBasic 3 - added logic to reserved_us_tld function

--- PythonBasics3/pythonBasics3.py
+++ PythonBasics3/pythonBasics3.py
@@ -44,6 +44,8 @@ def multiple_words(s):
 # Note: URLs will be in the format http://www.example.com. There will not be anything after the TLD
 # Hint: the URL should start with https and end with one of the TLDs
 def reserved_us_tld(s):
-  # YOUR CODE HERE
-
-  return
+  match = re.search("^https.*(.gov|.edu|.mil)$", s)
+  if (match == None):
+    return False
+  else:
+    return True
