a3a42cd2d66f590afa4c754e49df30083aee0d01
varunu311 <varunu311@gmail.com>
2022-10-26 23:49:13 -0400

    flask app #3 - added execution path for POST request for new note

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -2,7 +2,7 @@
 
 # imports
 import os                 # os is used to get environment variables IP & PORT
-from flask import Flask   # Flask is the web app that we will customize
+from flask import Flask,request   # Flask is the web app that we will customize
 from flask import render_template
 
 app = Flask(__name__)     # create an app
@@ -17,24 +17,30 @@ def index():
     return render_template('index.html', user = a_user)
 @app.route('/notes')
 def get_notes():
+    a_user = {'name': 'Varun', 'email': 'mogli@uncc.edu'}
     notes = {1: {'title':'First_note', 'text':'This is my first note', 'date':'10-1-2020'},
              2: {'title':'Second_note', 'text':'This is my second note', 'date':'10-2-2020'},
              3: {'title':'Third_note', 'text':'This is my third note', 'date':'10-3-2020'}
              }
-    return render_template('notes.html', notes=notes)
+    return render_template('notes.html', notes=notes,user = a_user)
 
-@app.route('/notes/new')
+@app.route('/notes/new', methods=['GET','POST'])
 def new_note():
+    print('request method is', request.method)
     a_user = {'name':'Mogli', 'email':'mogli@uncc.edu'}
-    return render_template('new.html', user=a_user)
+    if request.method == 'POST':
+        return '<h1> POST method used for this request </h1>'
+    else:
+        return render_template('new.html', user=a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
+    a_user = {'name':'Mogli', 'email':'mogli@uncc.edu'}
     notes = {1: {'title': 'First_note', 'text': 'This is my first note', 'date': '10-1-2020'},
              2: {'title': 'Second_note', 'text': 'This is my second note', 'date': '10-2-2020'},
              3: {'title': 'Third_note', 'text': 'This is my third note', 'date': '10-3-2020'}
              }
-    return render_template('note.html', note=notes[int(note_id)])
+    return render_template('note.html', note=notes[int(note_id)], user = a_user)
 
 @app.route('/user/<username>')
 def get_user(username):
