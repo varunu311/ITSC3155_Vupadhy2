d9cf6cce0719546de8e1e6fdc988884ac2b730a6
varunu311 <varunu311@gmail.com>
2022-11-15 12:56:05 -0500

    flask app #5 - edit not function handlespost and getD

--- flask_app/flask05.py
+++ flask_app/flask05.py
@@ -53,11 +53,28 @@ def new_note():
         a_user = db.session.query(User).filter_by(email='varunu311@gmail.com').one()
         return render_template('new.html', user=a_user)
 
-@app.route('/notes/edit/<note_id>')
+@app.route('/notes/edit/<note_id>', methods=['GET','POST'])
 def update_note(note_id):
-    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
-    return render_template('new.html',note=my_note,user=a_user)
+    if request.method == 'POST':
+        
+        title = request.form['title']
+        
+        text = request.form['noteText']
+        note = db.session.query(Note).filter_by(id=note_id).one()
+       
+        note.title = title
+        note.text = text
+
+        db.session.add(note)
+        db.session.commit()
+
+        return redirect(url_for('get_notes'))
+    else:
+        a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
+        
+        my_note = db.session.query(Note).filter_by(id=note_id).one()
+        
+        return render_template('new.html',note=my_note,user=a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
